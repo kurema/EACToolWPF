af4afff05b22a0a6d1d365ef2af374402d0a1910
kurema <kurema_makoto_software@yahoo.co.jp>
2024-03-19 23:38:40 +0900

    Open download

--- EACToolWpf/EACToolWpf/MainWindow.xaml.cs
+++ EACToolWpf/EACToolWpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,59 @@ namespace EACToolWpf
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Image_DragLeave(object sender, DragEventArgs e)
+		{
+		}
+
+		private void TextBox_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Enter)
+			{
+
+			}
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		string DownloadPath
+		{
+			get
+			{
+				var result = Vanara.PInvoke.Shell32.SHGetKnownFolderPath(new Guid("374DE290-123F-4565-9164-39C4925E467B"), 0, 0, out var path);
+				if (result == Vanara.PInvoke.HRESULT.S_OK && !string.IsNullOrEmpty(path)) return path;
+				return System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+			}
+		}
+
+		string DownloadImgPath=> System.IO.Path.Combine(DownloadPath, "Img");
+
+		private void Button_Click_OpenDownload(object sender, RoutedEventArgs e)
+		{
+
+			OpenOnExplorer(DownloadPath);
+		}
+
+		void OpenOnExplorer(string? directory)
+		{
+			if (string.IsNullOrWhiteSpace(directory)) return;
+			System.Diagnostics.ProcessStartInfo pinfo = new()
+			{
+				FileName = "explorer",
+				Arguments = directory
+			};
+			System.Diagnostics.Process.Start(pinfo);
+
+		}
+
+		private void Button_Click_CreateImg(object sender, RoutedEventArgs e)
+		{
+			if (System.IO.Directory.Exists(DownloadImgPath)) return;
+			System.IO.Directory.CreateDirectory(DownloadImgPath);
 		}
 	}
 }
